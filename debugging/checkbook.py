651ae0fd9e75b16033ca1d3be54767fc81e0e7e6
Maqa0024 <mahammadlimammadli@gmail.com>
2026-02-08 17:00:32 +0000

    Add checkbook.py placeholder script

---
+++ debugging/checkbook.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def main():
+    print("Checkbook script placeholder")
+    print("This is where transactions could be added or checked.")
+
+if __name__ == "__main__":
+    main()
